0b8393e98b96b58681db8b9b9bff0ff6bb02f510
Carlos Henrique <henriqueok20@gmail.com>
2019-04-18 22:52:28 -0400

    rotas de produtos finalizadas

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -18,7 +18,27 @@ exports.products_get_all = (req, res, next) => {
         });
 };
 
-exports.products_get_especific = (req, res, next) => {};
+exports.products_get_especific = (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+        .exec()
+        .then(product => {
+            if (product) {
+                res.status(200).json({
+                    product: product
+                });
+            } else {
+                res.status(404).json({
+                    message: "Sorry, we couldn't find any register for this id"
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
+};
 
 exports.products_post = (req, res, next) => {
     const product = new Product({
@@ -40,14 +60,61 @@ exports.products_post = (req, res, next) => {
         });
 };
 
+/* 
+atualiza o produto, a requisição deve seguir este modelo:
+[
+	{
+		"propName": "nome do campo a ser alterado",
+		"value": "valor que deverá substituir o atual"
+	}
+]
+*/
 exports.products_update = (req, res, next) => {
-    res.status(200).json({
-        message: "você atualizou um produto"
-    });
+    const id = req.params.productId;
+    const updateOperations = {};
+    console.log("body", req.body);
+    for (const operations of req.body) {
+        updateOperations[operations.propName] = operations.value;
+    }
+    Product.update({ _id: id }, { $set: updateOperations })
+        .exec()
+        .then(updateResult => {
+            if (product) {
+                res.status(200).json({
+                    message: "Product updated sucessfuly"
+                });
+            } else {
+                res.status(404).json({
+                    message: "Sorry, we couldn't find any register for this id"
+                });
+            }
+        })
+        .catch(error => {
+            console.log(err);
+            res.status(500).json({
+                error: error
+            });
+        });
 };
 
 exports.products_delete = (req, res, next) => {
-    res.status(200).json({
-        message: "você excluiu um produto"
-    });
+    const id = req.params.productId;
+    Product.deleteOne({ _id: id })
+        .exec()
+        .then(result => {
+            if (result) {
+                res.status(200).json({
+                    message: "Product deleted successfuly"
+                });
+            } else {
+                res.status(404).json({
+                    message: "Sorry, we couldn't find any register for this id"
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
 };
