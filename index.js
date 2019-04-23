0a9b0ee4c928e5e0477ba7a0a1dca43c42cba961
Carlos Henrique <henriqueok20@gmail.com>
2019-04-22 21:51:51 -0400

    trocando a porta

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const config = require("./src/config/config");
 const app = require("./src/app");
+const port = process.env.PORT || 3003;
 
 const server = http.createServer(app);
 
-server.listen(config.app);
+server.listen(port);
