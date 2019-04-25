da9d4fad81c7493bc05daeff26521d3c18ff0e76
Carlos Henrique <henriqueok20@gmail.com>
2019-04-25 17:26:18 -0400

    mudança no ponto inicial

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-/* const http = require("http");
-const config = require("./src/config/config");
-const app = require("./src");
-const PORT = process.env.PORT || 8081;
-
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
-    console.log(" rodando");
-});
- */
-
 const express = require("express");
 const config = require("./src/config/config");
 const app = require("./src");
@@ -19,5 +7,3 @@ const server = express();
 server.use(app);
 
 server.listen(config.app);
-
-//const server = http.createServer(app);
