d7a8cdb6afd6e5f2ec9e0fcee61d0af99009f53b
Carlos Henrique <henriqueok20@gmail.com>
2019-05-01 10:27:32 -0400

    arrumando os console.log

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -41,6 +41,7 @@ exports.products_get_especific = (req, res, next) => {
 };
 
 exports.products_post = async (req, res, next) => {
+    console.log("requisição", req);
     const { originalname: name, size, key, location: url = "" } = req.file;
 
     const postphoto = await PostPhoto.create({
@@ -50,7 +51,7 @@ exports.products_post = async (req, res, next) => {
         url
     });
 
-    console.log("postphoto", postphoto);
+    // console.log("postphoto", postphoto);
 
     const product = new Product({
         userWhoPostedId: req.body.userWhoPostedId,
@@ -69,7 +70,7 @@ exports.products_post = async (req, res, next) => {
     product
         .save()
         .then(prod_result => {
-            console.log("prod result", prod_result);
+            //  console.log("prod result", prod_result);
             Costumer.findByIdAndUpdate(
                 { _id: prod_result.userWhoPostedId },
                 { $push: { productsAcquired: prod_result._id } },
@@ -83,7 +84,7 @@ exports.products_post = async (req, res, next) => {
                     });
                 })
                 .catch(err => {
-                    console.log(err);
+                    //console.log(err);
                     res.status(500).json({
                         error: "erro ao add ao usuario" + err
                     });
@@ -135,14 +136,14 @@ exports.products_delete = async (req, res, next) => {
     /* const product = await  */ Product.findById(req.params.productId)
         .exec()
         .then(async resultado => {
-            console.log("resultado", resultado);
+            // console.log("resultado", resultado);
             const photo = await PostPhoto.findById(resultado.photo.photo_id);
             await photo.remove();
 
             Product.deleteOne({ _id: req.params.productId })
                 .exec()
                 .then(result => {
-                    console.log("result", result);
+                    //  console.log("result", result);
                     if (result) {
                         res.status(200).json({
                             message: "Product deleted successfuly"
