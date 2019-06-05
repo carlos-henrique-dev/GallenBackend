7d1db821871e8ab62330fb7dabbb0b0529ebf661
henriqueok20 <henriqueok20@gmail.com>
2019-06-05 12:23:51 -0400

    arrumando erro

--- src/routes/drugstores_routes.js
+++ src/routes/drugstores_routes.js
@@ -4,16 +4,14 @@ const authvalidation = require("../middlewares/auth_validation");
 
 const DrugstoreController = require("../controllers/drugstores_controller");
 
-router.post("/signup", authvalidation, DrugstoreController.drugstore_signup);
-
 router.delete("/:drugstoreID", authvalidation, DrugstoreController.drugstore_delete);
 
 router.patch("/:drugstoreID", authvalidation, DrugstoreController.drugstore_update);
 
 router.patch(
-    "/setAllnight/:drugstoreID",
-    authvalidation,
-    DrugstoreController.drugstore_setAllNight
+  "/setAllnight/:drugstoreID",
+  authvalidation,
+  DrugstoreController.drugstore_setAllNight
 );
 
 module.exports = router;
