2de23dc89b23643dce6d03056f2090db0181478c
henriqueok20 <henriqueok20@gmail.com>
2019-06-05 11:44:35 -0400

    tentando arrumr o update da farmacia

--- src/controllers/drugstores_controller.js
+++ src/controllers/drugstores_controller.js
@@ -1,39 +1,72 @@
-const bcrypt = require("bcrypt");
-
 const Drugstore = require("../models/drugstores_model");
-
-exports.drugstore_signup = (req, res, next) => {};
 
 exports.drugstore_delete = (req, res, next) => {};
 
-exports.drugstore_update = (req, res, next) => {};
+/* 
+atualiza o produto, a requisição deve seguir este modelo:
+[
+	{
+		"propName": "nome do campo a ser alterado",
+		"value": "valor que deverá substituir o atual"
+	}
+]
+*/
+
+// TODO: ver como atualizar uma foto
+exports.drugstore_update = (req, res, next) => {
+  console.log("body", req.body);
+  const id = req.params.drugstoreID;
+  const updateOperations = {};
+  for (const operations of req.body) {
+    updateOperations[operations.propName] = operations.value;
+  }
+  console.log("update", updateOperations);
+  /*   Drugstore.updateOne({ _id: id }, { $set: updateOperations })
+    .exec()
+    .then(updateResult => {
+      if (updateResult) {
+        res.status(200).json({
+          message: "Drugstore updated sucessfuly"
+        });
+      } else {
+        res.status(404).json({
+          message: "Sorry, we couldn't find any register for this id"
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "deu erro" + error
+      });
+    }); */
+};
 
 // params: [ { "propName": "allNight", "value": true/false } ]
 exports.drugstore_setAllNight = (req, res, next) => {
-    const id = req.params.drugstoreID;
-    const updateOperations = {};
+  const id = req.params.drugstoreID;
+  const updateOperations = {};
 
-    for (const operations of req.body) {
-        updateOperations[operations.propName] = operations.value;
-    }
+  for (const operations of req.body) {
+    updateOperations[operations.propName] = operations.value;
+  }
 
-    console.log("update", updateOperations);
-    Drugstore.updateOne({ _id: id }, { $set: updateOperations })
-        .exec()
-        .then(updateResult => {
-            if (updateResult) {
-                res.status(200).json({
-                    message: "status changed"
-                });
-            } else {
-                res.status(404).json({
-                    message: "Sorry, we couldn't find any register for this id "
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: "deu erro" + err
-            });
+  console.log("update", updateOperations);
+  Drugstore.updateOne({ _id: id }, { $set: updateOperations })
+    .exec()
+    .then(updateResult => {
+      if (updateResult) {
+        res.status(200).json({
+          message: "status changed"
         });
+      } else {
+        res.status(404).json({
+          message: "Sorry, we couldn't find any register for this id "
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "deu erro" + err
+      });
+    });
 };
